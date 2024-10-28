3dafb943412d3681e8101bf60e14bcf5504f4231
egnaro1007 <ndviettuan@gmai.com>
2024-10-29 01:15:18 +0700

    Refactor APIThanhToan class to change handling and data parsing

--- rate-exchange/main.py
+++ rate-exchange/main.py
@@ -1,5 +1,6 @@
 from banks import apithanhtoan
 from fastapi import FastAPI, HTTPException
+from banks.dataclasses import Bank, ExchangeRate
 
 app = FastAPI()
 
@@ -19,5 +20,25 @@ def get_banks():
 @app.get("/getexchangerate/{bank_short_name}")
 def get_exchange_rate(bank_short_name: str):
     if bank_short_name not in banks:
-        raise HTTPException(status_code=404, detail="Not supported")
-    return banks[bank_short_name].get_exchange_rate()
+        raise HTTPException(status_code=404, detail="Not Found")
+    try:
+        return banks[bank_short_name].get_exchange_rate()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/getexchangerates")
+def get_exchange_rates():
+    result = []
+    for bank in banks:
+        try:
+            result.append({
+                "success": True,
+                **banks[bank].get_exchange_rate()
+            })
+        except Exception as e:
+            result.append({
+                "success": False, 
+                "bank_name_short": bank,
+                "error": str(e)
+            })
+    return result
