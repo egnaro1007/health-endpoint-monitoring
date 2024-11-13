2d5ec718d2ef105e7d8f2bde25ea52d0143ee696
egnaro1007 <ndviettuan@gmai.com>
2024-11-13 22:01:11 +0700

    Embed metrics dashboard to web frontend

--- frontend/src/app/dashboard/metrics/page.js
+++ frontend/src/app/dashboard/metrics/page.js
@@ -9,12 +9,16 @@ export const metadata = {
 export default async function Page(props) {
   const query = 'metrics';
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl text-[#3b080f]`}>Metrics</h1>
       </div>
-      <div>
-        <iframe src="http://localhost:3000/d/EgnaroDAS/host-and-docker-monitoring?kiosk" title="Metrics Dashboard"></iframe>
+      <div className="w-full h-full">
+        <iframe 
+          src="/grafana/d/EgnaroDAS/host-and-docker-monitoring?kiosk" 
+          title="Metrics Dashboard"
+          className="w-full h-full border-none"
+        />
       </div>
     </div>
   );
